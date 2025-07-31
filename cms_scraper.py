3771ffa571304d92bf351e330a1dd62bb5038856
AiroGriefwind <130520211+AiroGriefwind@users.noreply.github.com>
2025-07-31 18:50:00 +0800

    2

--- cms_scraper.py
+++ cms_scraper.py
@@ -19,7 +19,7 @@ PASSWORD = os.getenv("CMS_PASSWORD")
 
 def main():
     chrome_options = Options()
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
